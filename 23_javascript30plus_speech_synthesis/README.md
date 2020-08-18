c6564a575f48866b49bc683874047465f7fdfab3
ALMaclaine <almaclaine@gmail.com>
2020-08-17 18:34:16 -0700

    finish 23_javascript30plus_speech_synthesis project

--- 23_javascript30plus_speech_synthesis/README.md
+++ 23_javascript30plus_speech_synthesis/README.md
@@ -3,3 +3,19 @@
 ## Changes
 
 - Move script into separate file
+
+- Remove `textarea` and `buttons` markup and styles
+
+- Replace `h1` style with `header` style, add it to `h1` tag
+
+- Add style `.voiceinator__slider` to sliders and replace `.voiceinator input` style
+with `.voiceinator .voiceinator__slider`
+
+- Add style `.voiceinator__voice-select` to dropdown and replace `.voiceinator select` style
+with `.voiceinator .voiceinator__voice-select` 
+
+- Add style `.voiceinator__message-input` to input and replace `.voiceinator input` style
+with `.voiceinator .voiceinator__message-input` 
+
+- Add style `.voiceinator__message-submit` to button and replace `.voiceinator button` style
+with `.voiceinator .voiceinator__message-submit` 
--- 23_javascript30plus_speech_synthesis/index.js
+++ 23_javascript30plus_speech_synthesis/index.js
@@ -1,10 +1,67 @@
+// Chatbot
+
+function ChatBot() {
+    /**
+     * respondTo
+     *
+     * return nothing to skip response
+     * return string for one response
+     * return array of strings for multiple responses
+     *
+     * @param input - input chat string
+     * @return reply of chat-bot
+     */
+    this.respondTo = function(input) {
+
+        this.input = input.toLowerCase();
+
+        if(this.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)'))
+            return "um... hi?";
+
+        if(this.match('what[^ ]* up') || this.match('sup') || this.match('how are you'))
+            return "this github thing is pretty cool, huh?";
+
+        if(this.match('l(ol)+') || this.match('(ha)+(h|$)') || this.match('lmao'))
+            return "what's so funny?";
+
+        if(this.match('^no+(\\s|!|\\.|$)'))
+            return "don't be such a negative nancy :(";
+
+        if(this.match('(cya|bye|see ya|ttyl|talk to you later)'))
+            return ["alright, see you around", "good teamwork!"];
+
+        if(this.match('(dumb|stupid|is that all)'))
+            return ["hey i'm just a proof of concept", "you can make me smarter if you'd like"];
+
+        if(this.input == 'noop')
+            return;
+
+        return input + " what?";
+    }
+
+    /**
+     * match
+     *
+     * @param regex - regex string to match
+     * @return boolean - whether or not the input string matches the regex
+     */
+    this.match = function(regex) {
+
+        return new RegExp(regex).test(this.input);
+    }
+}
+
+const bot = new ChatBot();
+
+console.log(bot.respondTo('hi'));
+console.log(bot.respondTo('hi'));
+
+// App
+
 const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
-msg.text = document.querySelector('[name="text"]').value;
 
 function populateVoices() {
     voices = this.getVoices();
@@ -35,5 +92,18 @@ function setOption() {
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
 options.forEach(option => option.addEventListener('change', setOption));
-speakButton.addEventListener('click', toggle);
-stopButton.addEventListener('click', () => toggle(false));
+
+const messageInput = document.getElementById('message-input');
+
+function handleMessage() {
+    const message = messageInput.value;
+    if(!message) return;
+    msg.text = bot.respondTo(message);
+    toggle();
+}
+
+document.getElementById('message-submit').addEventListener('click', handleMessage);
+
+window.addEventListener('keypress', (e) => {
+   if(e.key === 'Enter') handleMessage();
+});
